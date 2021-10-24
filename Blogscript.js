3341332a39d2cd54edcbcfced978bad7feda9e7f
Changat <40072115+Changat@users.noreply.github.com>
2021-10-24 15:22:10 +0100

    Create Blogscript.js

---
+++ Blogscript.js
@@ -0,0 +1,166 @@
+gdata.io.handleScriptLoaded({
+    "version": "1.0",
+    "encoding": "UTF-8",
+    "feed": {
+        "xmlns": "http://www.w3.org/2005/Atom",
+        "xmlns$openSearch": "http://a9.com/-/spec/opensearchrss/1.0/",
+        "xmlns$gd": "http://schemas.google.com/g/2005",
+        "id": {
+            "$t": "tag:blogger.com,1999:blog-6564966195710952198"
+        },
+        "updated": {
+            "$t": "2021-10-24T15:11:15.286+02:00"
+        },
+        "title": {
+            "type": "text",
+            "$t": "قالب سكويز – قالب بلوجر عصري للمجلات"
+        },
+        "subtitle": {
+            "type": "html",
+            "$t": ""
+        },
+        "link": [{
+            "rel": "http://schemas.google.com/g/2005#feed",
+            "type": "application/atom+xml",
+            "href": "https://www.blogger.com/feeds/6564966195710952198/pages/default?alt=json-in-script"
+        }, {
+            "rel": "self",
+            "type": "application/atom+xml",
+            "href": "https://www.blogger.com/feeds/6564966195710952198/pages/default?alt=json-in-script"
+        }, {
+            "rel": "alternate",
+            "type": "text/html",
+            "href": "http://www.squeeze-template.com/"
+        }],
+        "author": [{
+            "name": {
+                "$t": "Squeeze Template"
+            },
+            "uri": {
+                "$t": "https://www.blogger.com/profile/03008047176837947156"
+            },
+            "email": {
+                "$t": "noreply@blogger.com"
+            },
+            "gd$image": {
+                "rel": "http://schemas.google.com/g/2005#thumbnail",
+                "width": "32",
+                "height": "32",
+                "src": "//4.bp.blogspot.com/-CJvZ2lzhMwA/XLQM5BvhbMI/AAAAAAAAAqk/Nq7Gb2tue2Q6qaOIl9Z2FkJ6owlCM0ldACK4BGAYYCw/s32/Thumb.png"
+            }
+        }],
+        "generator": {
+            "version": "7.00",
+            "uri": "https://www.blogger.com",
+            "$t": "Blogger"
+        },
+        "openSearch$totalResults": {
+            "$t": "2"
+        },
+        "openSearch$startIndex": {
+            "$t": "1"
+        },
+        "openSearch$itemsPerPage": {
+            "$t": "25"
+        },
+        "entry": [{
+            "id": {
+                "$t": "tag:blogger.com,1999:blog-6564966195710952198.page-3314844930647801555"
+            },
+            "published": {
+                "$t": "2020-09-22T14:52:00.002+02:00"
+            },
+            "updated": {
+                "$t": "2020-09-22T14:52:53.770+02:00"
+            },
+            "title": {
+                "type": "text",
+                "$t": "CommentsSystem"
+            },
+            "content": {
+                "type": "html",
+                "$t": ""
+            },
+            "link": [{
+                "rel": "edit",
+                "type": "application/atom+xml",
+                "href": "https://www.blogger.com/feeds/6564966195710952198/pages/default/3314844930647801555"
+            }, {
+                "rel": "self",
+                "type": "application/atom+xml",
+                "href": "https://www.blogger.com/feeds/6564966195710952198/pages/default/3314844930647801555"
+            }, {
+                "rel": "alternate",
+                "type": "text/html",
+                "href": "http://www.squeeze-template.com/p/commentssystem.html",
+                "title": "CommentsSystem"
+            }],
+            "author": [{
+                "name": {
+                    "$t": "Squeeze Template"
+                },
+                "uri": {
+                    "$t": "https://www.blogger.com/profile/03008047176837947156"
+                },
+                "email": {
+                    "$t": "noreply@blogger.com"
+                },
+                "gd$image": {
+                    "rel": "http://schemas.google.com/g/2005#thumbnail",
+                    "width": "32",
+                    "height": "32",
+                    "src": "//4.bp.blogspot.com/-CJvZ2lzhMwA/XLQM5BvhbMI/AAAAAAAAAqk/Nq7Gb2tue2Q6qaOIl9Z2FkJ6owlCM0ldACK4BGAYYCw/s32/Thumb.png"
+                }
+            }]
+        }, {
+            "id": {
+                "$t": "tag:blogger.com,1999:blog-6564966195710952198.page-4863414686232101325"
+            },
+            "published": {
+                "$t": "2019-01-04T18:28:00.000+02:00"
+            },
+            "updated": {
+                "$t": "2019-05-16T20:46:04.349+02:00"
+            },
+            "title": {
+                "type": "text",
+                "$t": "scripts"
+            },
+            "content": {
+                "type": "html",
+                "$t": "\r "
+            },
+            "link": [{
+                "rel": "edit",
+                "type": "application/atom+xml",
+                "href": "https://www.blogger.com/feeds/6564966195710952198/pages/default/4863414686232101325"
+            }, {
+                "rel": "self",
+                "type": "application/atom+xml",
+                "href": "https://www.blogger.com/feeds/6564966195710952198/pages/default/4863414686232101325"
+            }, {
+                "rel": "alternate",
+                "type": "text/html",
+                "href": "http://www.squeeze-template.com/p/scripts.html",
+                "title": "scripts"
+            }],
+            "author": [{
+                "name": {
+                    "$t": "Squeeze Template"
+                },
+                "uri": {
+                    "$t": "https://www.blogger.com/profile/03008047176837947156"
+                },
+                "email": {
+                    "$t": "noreply@blogger.com"
+                },
+                "gd$image": {
+                    "rel": "http://schemas.google.com/g/2005#thumbnail",
+                    "width": "32",
+                    "height": "32",
+                    "src": "//4.bp.blogspot.com/-CJvZ2lzhMwA/XLQM5BvhbMI/AAAAAAAAAqk/Nq7Gb2tue2Q6qaOIl9Z2FkJ6owlCM0ldACK4BGAYYCw/s32/Thumb.png"
+                }
+            }]
+        }]
+    }
+});
